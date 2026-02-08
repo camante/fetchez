db9c605c91a78d4b17623102720f7f75be53045c
Matthew Love <matthew.love@colorado.edu>
2026-02-07 20:41:26 -0800

    to_geo_transform in spatial.Region

--- src/fetchez/spatial.py
+++ src/fetchez/spatial.py
@@ -252,7 +252,6 @@ class Region:
         tuple: (top_left_x, x_res, 0, top_left_y, 0, -y_res)
         """
     
-        min_x, min_y, max_x, max_y = region
         x_res = (self.xmax - self.xmin) / float(nx)
         y_res = (self.ymax - self.ymin) / float(ny)
         return (self.xmin, x_res, 0, self.ymax, 0, -y_res)
